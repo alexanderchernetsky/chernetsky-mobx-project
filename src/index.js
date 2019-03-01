6894d3cb940e5b2369701f4b4a02e63bebb0c961
Alexander Chernetsky <alexvnderchernetsky@gmail.com>
2019-03-01 18:30:00 +0300

    Delete comments from index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-/*import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
-import '../node_modules/@fortawesome/fontawesome-free/js/all';*/
 import './style.sass';
 import App from './App';
 
